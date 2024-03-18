616957225f6af79ca70b2ca84c5e34bbc5212905
pradeepb007 <bpradeep34@gmai.com>
2024-03-18 07:24:06 +0530

    api interceptors updated

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -7,6 +7,33 @@ const api = axios.create({
   timeout: 10000, // adjust as needed
 });
 
+// Add a request interceptor
+api.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    console.error("Request error:", error);
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+api.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
 export const performApiRequest = async (url, method = "GET", data = null) => {
   try {
     const response = await api.request({
